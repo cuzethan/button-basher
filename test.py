64b1147380d6f4509685569bc29006bad6de2492
TarifEKhan <147672925+TarifEKhan@users.noreply.github.com>
2024-10-25 10:09:33 -0700

    New button
    New button

--- test.py
+++ test.py
@@ -8,18 +8,25 @@ BUTTON_IMAGE = "button.png"
 
 class ButtonClickerGame(arcade.Window):
     def __init__ (self):
-        super().__init__(SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.ASH_GREY)
+        self.button_sprite = None
 
     def setup(self):
-        pass
-
+        self.button_sprite = arcade.Sprite(BUTTON_IMAGE, scale = 0.2)
+        self.button_sprite.center_x = SCREEN_WIDTH // 2
+        self.button_sprite.center_y = SCREEN_HEIGHT // 2
 
     def on_draw(self):
-        arcade.start_render();
+        arcade.start_render()
+        self.button_sprite.draw()
 
     def on_update(self, delta_time):
         pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.button_sprite.collides_with_point((x,y)):
+            print ("Button Clicked!")
 
 def main():
     game = ButtonClickerGame()
