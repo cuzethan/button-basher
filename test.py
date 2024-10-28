caec6a2d93bd01a39a2691285c04b9f4cc7aaff1
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-10-27 20:37:19 -0700

    integrated Tarif's changes into main

--- test.py
+++ test.py
@@ -2,13 +2,10 @@ import random
 import arcade
 
 # --- Constants ---
-SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = .25
-COIN_COUNT = 50
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Implement Views Example"
+SCREEN_TITLE = "BUTTON BASHER"
+BUTTON_IMAGE = "button.png"
 
 
 class GameView(arcade.View):
@@ -20,79 +17,32 @@ class GameView(arcade.View):
         super().__init__()
 
         # Variables that will hold sprite lists
-        self.player_list = None
-        self.coin_list = None
+        self.button_sprite = None
 
         # Set up the player info
-        self.player_sprite = None
         self.score = 0
 
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
         """ Set up the game and initialize the variables. """
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-
-        # Score
-        self.score = 0
-
-        # Set up the player
-        # Character image from kenney.nl
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
-                                           SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
-
-        # Create the coins
-        for i in range(COIN_COUNT):
-
-            # Create the coin instance
-            # Coin image from kenney.nl
-            coin = arcade.Sprite(":resources:images/items/coinGold.png",
-                                 SPRITE_SCALING_COIN)
-
-            # Position the coin
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
-
-            # Add the coin to the lists
-            self.coin_list.append(coin)
+        self.button_sprite = arcade.Sprite(BUTTON_IMAGE)
+        self.button_sprite.center_x = SCREEN_WIDTH // 2
+        self.button_sprite.center_y = SCREEN_HEIGHT // 2
 
     def on_draw(self):
         """ Draw everything """
-        self.clear()
-        self.coin_list.draw()
-        self.player_list.draw()
-
-        # Put the text on the screen.
-        output = f"Score: {self.score}"
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Handle Mouse Motion """
-
-        # Move the center of the player sprite to match the mouse x, y
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
+        arcade.start_render()
+        self.button_sprite.draw()
+        arcade.draw_text(f"Score: {self.score}", 10, SCREEN_HEIGHT-30, arcade.color.BLACK, 20)
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
-
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        self.coin_list.update()
-
-        # Generate a list of all sprites that collided with the player.
-        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
-
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for coin in coins_hit_list:
-            coin.remove_from_sprite_lists()
+        pass
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.button_sprite.collides_with_point((x,y)):
             self.score += 1
+            print (f"Button Clicked! Current score: {self.score}")
 
 class StartView(arcade.View):
     def on_show_view(self):
