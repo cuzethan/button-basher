b51e40b7a3a6deb4954b9f1a8f8c62b3c1b128cc
Ethan Le <ethanisverybusyrightnow@gmail.com>
2024-11-11 15:24:05 -0800

    shifted button side to right

--- game_view.py
+++ game_view.py
@@ -1,7 +1,5 @@
 import arcade
-
-#constants
-BUTTON_IMAGE = "button.png"
+from button_section import ButtonSection
 
 class GameView(arcade.View):
     """ Our custom Window Class"""
@@ -11,30 +9,15 @@ class GameView(arcade.View):
         # Call the parent class initializer
         super().__init__()
 
-        # Variables that will hold sprite lists
-        self.button_sprite = None
-
-        # Set up the player info
-        self.score = 0
-
-        arcade.set_background_color(arcade.color.AMAZON)
+        self.add_section(ButtonSection(self.window.width / 2, 0,
+                                    self.window.width / 2, self.window.height,
+                                    name='Button'))
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
-        self.button_sprite = arcade.Sprite(BUTTON_IMAGE)
-        self.button_sprite.center_x = self.window.width // 2 
-        self.button_sprite.center_y = self.window.height // 2
+        pass
 
     def on_draw(self):
-        """ Draw everything """
         arcade.start_render()
-        self.button_sprite.draw()
-        arcade.draw_text(f"Score: {self.score}", 10, self.window.height-30, arcade.color.BLACK, 20)
 
     def on_update(self, delta_time):
         pass
-    
-    def on_mouse_press(self, x, y, button, modifiers):
-        if self.button_sprite.collides_with_point((x,y)):
-            self.score += 1
-            print (f"Button Clicked! Current score: {self.score}")
