437db1cc3510aa39d24e12d65172c51a88e328e4
cuzethan <leethan444@gmail.com>
2024-11-30 09:44:42 -0800

    deleted unnecessary code in game_view.py and centralized all access data from button_section.py into game_view.py

--- views/game_view.py
+++ views/game_view.py
@@ -5,29 +5,16 @@ from .upgrades import AutoClicker, DoubleClicker
 class GameView(arcade.View):
     def __init__(self, name):
         super().__init__()
-        self.name = name
-        self.score = 0
+        self.name = name #factory name
+        self.score = 0 
+        self.score_per_sec = 0
         self.click_value = 1
         self.auto_clicker = AutoClicker()
         self.double_clicker = DoubleClicker()
 
         # Store ButtonSection as an attribute to access it in UpgradeSection
-        self.button_section = ButtonSection(self.window.width / 2, 0, self.window.width / 2, self.window.height, name=self.name)
+        self.button_section = ButtonSection(self.window.width / 2, 0, self.window.width / 2, self.window.height, name=self.name, game_view=self)
         self.add_section(self.button_section)
         
         # Pass `self` as the game_view argument to UpgradeSection
         self.add_section(UpgradeSection(0, 0, self.window.width / 2, self.window.height, game_view=self))
-
-
-
-    def setup(self):
-        pass
-
-    def on_draw(self):
-        arcade.start_render()
-
-    def on_update(self, delta_time):
-        self.auto_clicker.apply(self)
-    def on_mouse_press(self, x, y, button, modifiers):
-        # Update score by click value (1 or 2 based on DoubleClicker upgrade)
-        self.score += self.click_value
--- views/sections/button_section.py
+++ views/sections/button_section.py
@@ -5,8 +5,10 @@ BUTTON_IMAGE = "assets/button.png"
 
 class ButtonSection(arcade.Section):
 
-    def __init__(self, left: int, bottom: int, width: int, height: int, **kwargs):
+    def __init__(self, left: int, bottom: int, width: int, height: int, game_view, **kwargs):
         super().__init__(left, bottom, width, height, **kwargs)
+
+        self.game_view = game_view #will give us access to all data
 
         # Variables that will hold sprite lists
         """ Set up the game and initialize the variables. """
@@ -19,24 +21,19 @@ class ButtonSection(arcade.Section):
         self.scale_target = 1.0  # Target scale for animation
         self.scale_speed = 5.0  # Speed of scaling animation
 
-        # Set up the player info
-        self.score = 0
-        self.score_per_sec = 0
-        self.click_value = 1
-
     def on_draw(self):
         arcade.draw_lrtb_rectangle_filled(self.left, self.right, self.top, self.bottom, arcade.color.GRAY)
         self.button_sprite.draw()
         arcade.draw_text(f"{self.name}'s Button Factory", self.left, self.window.height - 60, 
             arcade.color.BLACK, 25, width=self.width, align="center")
-        arcade.draw_text(f"{int(self.score)} Buttons", self.left, self.window.height // 2 + 200, 
+        arcade.draw_text(f"{int(self.game_view.score)} Buttons", self.left, self.window.height // 2 + 200, 
             arcade.color.BLACK, 25, width=self.width, align="center")
-        arcade.draw_text(f"{self.score_per_sec} Buttons Per Sec", self.left, self.window.height // 2 + 160, 
+        arcade.draw_text(f"{self.game_view.score_per_sec} Buttons Per Sec", self.left, self.window.height // 2 + 160, 
             arcade.color.BLACK, 20, width=self.width, align="center")
         
     def on_update(self, delta_time):
         # Increment score automatically based on score_per_sec
-        self.score += self.score_per_sec * delta_time
+        self.game_view.score += self.game_view.score_per_sec * delta_time
 
         # Smoothly scale the button sprite
         if self.button_sprite.scale != self.scale_target:
@@ -55,5 +52,5 @@ class ButtonSection(arcade.Section):
 
     def on_mouse_press(self, x, y, button, modifiers):
         if self.button_sprite.collides_with_point((x, y)):
-            self.score += self.click_value
+            self.game_view.score += self.game_view.click_value
             self.scale_target = 0.9  # Shrink the button when clicked
