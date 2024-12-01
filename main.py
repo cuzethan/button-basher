18bbfbd9afbc92fc933fbd73b6dfb076417df152
Artyom <artyom.liakhavets@sjsu.edu>
2024-11-30 18:50:39 -0800

    Center window on spawn

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ SCREEN_TITLE = "BUTTON BASHER"
 def main():
     """ Main function """
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.center_window()
     start_view = StartView()
     window.show_view(start_view)
     arcade.run()
