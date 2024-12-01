5fabe6d4acf26cb445c37f92162a8349addd58bb
cuzethan <leethan444@gmail.com>
2024-11-30 16:19:01 -0800

    refactored upgrades into lists and for loops for simplicity, fixed clicking on upgrades and boxed them up

--- views/sections/upgrade_section.py
+++ views/sections/upgrade_section.py
@@ -7,47 +7,28 @@ class UpgradeSection(arcade.Section):
         super().__init__(left, bottom, width, height, **kwargs)
         self.game_view = game_view
 
-        #All upgrades
-        self.auto_clicker = AutoClicker()
-        self.double_clicker = DoubleClicker()
-        self.mega_auto_clicker = MegaAutoClicker()
-        self.factory_workers = FactoryWorker()
+        #Upgrades list, each tuple contains upgrade and y_offset
+        self.upgrades = [
+            (AutoClicker(), 100),
+            (DoubleClicker(), 150),
+            (MegaAutoClicker(), 200),
+            (FactoryWorker(), 250)
+        ]
         
     def on_draw(self):
         arcade.draw_lrtb_rectangle_filled(self.left, self.right, self.top, self.bottom, arcade.color.ORANGE)
         arcade.draw_text("Upgrades", self.left, self.window.height - 60, 
                         arcade.color.BLACK, 25, width=self.width, align="center")
 
-        # AutoClicker Upgrade
-        arcade.draw_text(self.auto_clicker.getDesc(), self.left + 20, self.top - 100, 
-                        self.auto_clicker.getColor(), 16)
-
-        # DoubleClicker Upgrade
-        arcade.draw_text(self.double_clicker.getDesc(), self.left + 20, self.top - 140, 
-                        self.double_clicker.getColor(), 16)
-        
-        # Draw Mega Autoclicker Upgrade
-        arcade.draw_text(self.mega_auto_clicker.getDesc(), self.left + 20, self.top - 180, 
-                        self.mega_auto_clicker.getColor(), 16)
-
-        # Draw Factory Worker Upgrade
-        arcade.draw_text(self.factory_workers.getDesc(), self.left + 20, self.top - 220, 
-                        self.factory_workers.getColor(), 16)
+        # Iterate through upgrades and draw them dynamically
+        for upgrade, y_offset in self.upgrades:
+            arcade.draw_text(upgrade.getDesc(), self.left + 20, self.top - y_offset, 
+                        upgrade.getColor(), 16)
+            arcade.draw_lrtb_rectangle_outline(self.left + 10, self.right - 10, self.top - (y_offset - 25),
+                        self.top - (y_offset + 7.5), arcade.color.BLACK, 2)
 
     def on_mouse_press(self, x, y, button, modifiers):
-        # Check for AutoClicker purchase
-        if (self.left + 20 < x < self.left + 180 and self.top - 110 < y < self.top - 90):
-            self.auto_clicker.activate(self.game_view) # activate upgrade
-
-        # Check for DoubleClicker purchase
-        if (self.left + 20 < x < self.left + 180 and self.top - 150 < y < self.top - 130):
-            self.double_clicker.activate(self.game_view) # activate upgrade
-
-        # Check for MegaAutoClicker purchase
-        if (self.left + 20 < x < self.left + 180 and self.top - 190 < y < self.top - 170):
-            self.mega_auto_clicker.activate(self.game_view) # activate upgrade
-
-        # Check for Factory Worker purchase
-        if (self.left + 20 < x < self.left + 180 and self.top - 230 < y < self.top - 210):
-            self.factory_workers.activate(self.game_view)
-
+        # Iterate through upgrades and check if they were clicked
+        for upgrade, y_offset in self.upgrades:
+            if (self.left + 10 < x < self.right - 10 and self.top - (y_offset + 7.5) < y < self.top - (y_offset - 25)):
+                upgrade.activate(self.game_view)
